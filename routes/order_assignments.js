2bb3852f9c08a4ee39cf3f4a156340d72b8a5b19
Christina <christina83@users.noreply.github.com>
2022-02-22 23:46:38 +0100

    order assignments dropdowns finished

--- routes/order_assignments.js
+++ routes/order_assignments.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const { Order_assignment, getAllAssignments } = require('../models/Order_assignment');
+const { getAllMachines } = require('../models/Machine');
+const { getAllOrders } = require('../models/Order');
 
 router.get('/', async (req, res) => {
-  const result = await getAllAssignments();
-  res.render('pages/order_assignments', {
-    order_assignments: result.rows
-  });
+    const assignments = await getAllAssignments();
+    const machines = await getAllMachines(); 
+    const orders = await getAllOrders();
+    res.render('pages/order_assignments', {
+        order_assignments: assignments.rows,
+        dropdown_machines: machines.rows,
+        dropdown_orders: orders.rows
+    });
 });
 
+
+
 router.post // Hier überlegen, wie ein neues Assignment erstellt werden soll (Dropdown anbieten mit bestehenden Maschinen/Orders)
+
 
 module.exports = router;
